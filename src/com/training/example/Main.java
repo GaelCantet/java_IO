b6f581d4ebe671c47969e679a6b73b2ae82b261d
GCantet <gael.cantet@orange.fr>
2022-02-09 11:26:41 +0100

    boucle lecture fichier

--- src/com/training/example/Main.java
+++ src/com/training/example/Main.java
@@ -1,8 +1,31 @@
 package com.training.example;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner scan = new Scanner(System.in);
+        boolean isOk = false;
+        while (!isOk) {
+            try {
+                System.out.println("Quel est le chemin de votre fichier ?");
+                String path = scan.nextLine();
+                Reader fileReader = new FileReader(path);
+                int data = fileReader.read();
+                while (data != -1) {
+                    System.out.println(data);
+                    data = fileReader.read();
+                }
+                isOk = true;
+            } catch (InputMismatchException | IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
     }
 }
